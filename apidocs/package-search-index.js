9da2897fa8661d70ac817dfd68bc0ee3089c2053
Mark Bendarczyk <mark@slytechs.com>
2026-04-13 20:52:24 -0400

    docs: update Javadocs for 3.0.0

--- apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.slytechs.jnet.jnetpcap","l":"com.slytechs.jnet.jnetpcap"},{"m":"com.slytechs.jnet.jnetpcap","l":"com.slytechs.jnet.jnetpcap.internal"}];updateSearchResults();
--- apidocs/tag-search-index.js
+++
@@ -1,1 +0,0 @@
-tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"},{"l":"IPF Modes","h":"class com.slytechs.jnet.jnetpcap.NetPcap","d":"Section","u":"com.slytechs.jnet.jnetpcap/com/slytechs/jnet/jnetpcap/NetPcap.html#ipf-modes-heading"}];updateSearchResults();
